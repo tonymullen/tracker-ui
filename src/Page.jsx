863022024761c6abce1e66ca09db2541d0b5309a
Tony Mullen <tony.mullen@gmail.com>
2020-03-16 12:00:12 -0700

    First commit

---
+++ src/Page.jsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import {
+  Navbar, Nav, NavItem, NavDropdown,
+  MenuItem, Glyphicon,
+  Grid, Col,
+} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+import IssueAddNavItem from './IssueAddNavItem.jsx';
+import Contents from './Contents.jsx';
+import Search from './Search.jsx';
+import UserContext from './UserContext.js';
+import graphQLFetch from './graphQLFetch.js';
+import store from './store.js';
+import SignInNavItem from './SignInNavItem.jsx';
+
+function NavBar({ user, onUserChange }) {
+  return (
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>Issue Tracker</Navbar.Brand>
+      </Navbar.Header>
+      <Nav>
+        <LinkContainer exact to="/">
+          <NavItem>Home</NavItem>
+        </LinkContainer>
+        <LinkContainer to="/issues">
+          <NavItem>Issue List</NavItem>
+        </LinkContainer>
+        <LinkContainer to="/report">
+          <NavItem>Report</NavItem>
+        </LinkContainer>
+      </Nav>
+      <Col sm={5}>
+        <Navbar.Form>
+          <Search />
+        </Navbar.Form>
+      </Col>
+      <Nav pullRight>
+        <IssueAddNavItem user={user} />
+        <SignInNavItem user={user} onUserChange={onUserChange} />
+        <NavDropdown
+          id="user-dropdown"
+          title={<Glyphicon glyph="option-vertical" />}
+          noCaret
+        >
+          <LinkContainer to="/about">
+            <MenuItem>About</MenuItem>
+          </LinkContainer>
+        </NavDropdown>
+      </Nav>
+    </Navbar>
+  );
+}
+
+function Footer() {
+  return (
+    <small>
+      <hr />
+      <p className="text-center">
+        Full source code available at this
+        {' '}
+        <a href="https://github.com/tonymullen/ProMERNStackBook">
+          GitHub repository
+        </a>
+      </p>
+    </small>
+  );
+}
+
+export default class Page extends React.Component {
+  static async fetchData(cookie) {
+    const query = `query { user {
+      signedIn givenName
+    }}`;
+    const data = await graphQLFetch(query, null, null, cookie);
+    return data;
+  }
+
+  constructor(props) {
+    super(props);
+    const user = store.userData ? store.userData.user : null;
+    delete store.userData;
+    this.state = { user };
+    this.onUserChange = this.onUserChange.bind(this);
+  }
+
+  async componentDidMount() {
+    // const apiEndpoint = window.ENV.UI_AUTH_ENDPOINT;
+    // const response = await fetch(`${apiEndpoint}/user`, {
+    //   method: 'POST',
+    //   credentials: 'include',
+    // });
+    // const body = await response.text();
+    // const result = JSON.parse(body);
+    // const { signedIn, givenName } = result;
+    // this.setState({ user: { signedIn, givenName } });
+    const { user } = this.state;
+    if (user == null) {
+      const data = await Page.fetchData();
+      this.setState({ user: data.user });
+    }
+  }
+
+  onUserChange(user) {
+    this.setState({ user });
+  }
+
+  render() {
+    const { user } = this.state;
+    if (user == null) return null;
+    return (
+      <div>
+        <NavBar user={user} onUserChange={this.onUserChange} />
+        <Grid fluid>
+          <UserContext.Provider value={user}>
+            <Contents />
+          </UserContext.Provider>
+        </Grid>
+        <Footer />
+      </div>
+    );
+  }
+}
